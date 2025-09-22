4bd32d895e0d011d6486ef569fb2b662af7e2497
AdinathIngole <adiingole1006@gmail.com>
2025-09-22 20:39:26 +0530

    intial commit

---
+++ src/Constant.js
@@ -0,0 +1,250 @@
+// Skills Section Logo's
+import htmlLogo from './assets/Tech_Logo/html.png';
+import cssLogo from './assets/Tech_Logo/css.png';
+import bootstrapLogo from './assets/Tech_Logo/bootstrap.png';
+import cppLogo from './assets/Tech_Logo/cpp.png';
+import javaLogo from './assets/Tech_Logo/java.png';
+import javascriptLogo from './assets/Tech_Logo/javascript.png';
+import mysqlLogo from './assets/Tech_Logo/mysql.png';
+import reactjsLogo from './assets/Tech_Logo/reactjs.png';
+import springbootLogo from './assets/Tech_Logo/springboot.png';
+import tailwindcssLogo from './assets/Tech_Logo/tailwindcss.png';
+import vercelLogo from './assets/Tech_Logo/vercel.png';
+import postmanLogo from './assets/Tech_Logo/postman.png';
+import vscodeLogo from './assets/Tech_Logo/vscode.png';
+import gitLogo from './assets/Tech_Logo/git.png';
+import githubLogo from './assets/Tech_Logo/github.png';
+import FergussonLogo from './assets/Tech_Logo/FergussonLogo.jpeg';
+import ServiceHubLogo from './assets/Project_Logo/ServiceHubLogo.png';
+import eclipseLogo from './assets/Tech_Logo/eclipse-logo.png';
+import MHStateBoardLogo from './assets/Tech_Logo/MHStateBoardLogo.png';
+import DPULogo from './assets/Tech_Logo/DPULogo.jpg';
+import KaushalyaTechLogo from './assets/Tech_Logo/KaushalyaTechLogo.png';
+import NexappLogo from './assets/Tech_Logo/NexappLogo.jpeg';
+import IVLogo from './assets/Tech_Logo/IVLogo.jpeg';
+import HotelManagementLogo from './assets/Project_Logo/HotelManagement.png';
+import CompanyLogo from './assets/Project_Logo/CompanyLogo.png';
+import HRMSLogo from './assets/Project_Logo/HRMS.png';
+import EventManagementLogo from './assets/Project_Logo/EventManagementLogo.png';
+import LibraryLogo from './assets/Project_Logo/LibraryLogo.png';
+import MatrimonyLogo from './assets/Project_Logo/MatrimonyLogo.png';
+import SSMFNGOLOGO from './assets/Project_Logo/SSMFNGO.png';
+
+export const SkillsInfo = [
+  {
+    title: 'Frontend',
+    skills: [
+      { name: 'HTML', logo: htmlLogo },
+      { name: 'CSS', logo: cssLogo },
+      { name: 'Java-Script', logo: javascriptLogo },
+      { name: 'React JS', logo: reactjsLogo },
+      { name: 'Tailwind CSS', logo: tailwindcssLogo },
+      { name: 'Bootstrap', logo: bootstrapLogo },
+    ],
+  },
+  {
+    title: 'Backend',
+    skills: [
+      { name: 'Spring Boot', logo: springbootLogo },
+      { name: 'MySQL', logo: mysqlLogo },
+    ],
+  },
+  {
+    title: 'Languages',
+    skills: [
+      { name: 'C++', logo: cppLogo },
+      { name: 'Java', logo: javaLogo },
+      { name: 'Java-Script', logo: javascriptLogo },
+    ],
+  },
+  {
+    title: 'Tools',
+    skills: [
+      { name: 'Git', logo: gitLogo },
+      { name: 'GitHub', logo: githubLogo },
+      { name: 'VS Code', logo: vscodeLogo },
+      { name: 'Postman', logo: postmanLogo },
+      { name: 'Eclipse', logo: eclipseLogo },
+    ],
+  },
+];
+
+  export const experiences = [
+    {
+      id: 0,
+      img: KaushalyaTechLogo,
+      role: "Full-Stack Developer",
+      company: "Kaushalya Tech Solutions",
+      date: "Jan-2024 - Jun-2025",
+      exp: "1 yr 6 mos",
+      desc: "Developed dynamic and scalable web applications using the Java Spring-boot and Reactjs , handling both frontend and backend development. Collaborated with cross-functional teams to build responsive UI, implement RESTful APIs, and optimize application performance in an agile environment.",
+      skills: [
+        "HTML",
+        "CSS",
+        "JavaScript",
+        "React JS",
+        "Java",
+        "Spring-boot",
+        "Spring-Security",
+        "MySQL",
+        "Hibernate",
+        "JDBC",
+        "JWT",
+      ],
+    },
+    {
+      id: 1,
+      img: NexappLogo,
+      role: "Software Developer (Trainee)",
+      company: "Nexapp Technologies pvt ltd",
+      date: "Jun 2023 - Sep 2023",
+      exp: "4 month",
+      desc: "Contributed to innovative projects as a Software Engineer, leading both frontend and backend development using technologies such as HTML, CSS, JavaScript, MySQL, Bootstrap, and ReactJS. Worked closely with the team to deliver responsive, high-performance web applications and improve user experience through seamless integration of various technologies.",
+      skills: [
+        "ReactJS",
+        "JavaScript",
+        "Bootstrap",
+        "HTML",
+        "CSS",
+        "MySQL",
+      ],
+    },
+    {
+      id: 2,
+      img: IVLogo,
+      role: "Software Developer (Internship)",
+      company: "IntelliVision Technologies pvt ltd",
+      date: "Mar 2022 - Sep 2022",
+      exp: "7 month",
+      desc: "I worked on the VMDSERVICES project, contributing to its development and implementation.Through my active involvement, I gained hands-on experience in software development and successfully contributed to the project's objectives. leading both frontend and backend development using technologies such as C++, HTML, CSS, JavaScript and MySQL.", 
+      skills: [
+        "C++",
+        "HTML",
+        "CSS",
+        "JavaScript",
+        "MySQL",
+      ],
+    },
+  ];
+  
+  export const education = [
+    {
+      id: 0,
+      img: FergussonLogo,
+      school: "Fergusson College (Autonomous), Pune 411004",
+      date: "June 2019 - June 2022",
+      grade: "8.5 CGPA",
+      desc: "I have completed my Master's degree (MSc) in Computer Applications  with Industrial Mathematics from  Fergusson College Pune. During my time at Fergusson College, I gained a strong foundation in programming, software development, and computer science principles. I have studied courses such as Java, Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Web Development, and Mathematics concept.",
+      degree: "Master of Science (Industrial Mathematics With Computer Applications)",
+    },
+    {
+      id: 1,
+      img: DPULogo,
+      school: "Dr D Y Patil Arts Commerce And Science College Pimpri Pune",
+      date: "July 2015 - June 2018",
+      grade: "61.90%",
+      desc: "I completed my Bachelor's degree in B.Sc Mathematics from Dr D Y Patil College, Pimpri. Throughout my studies, I was immersed in a variety of subjects that deepened my understanding of Mathematics Concepts. My time at D Y Patil College allowed me to work on projects that applied Mathematical Algorithm and theoretical concepts to real-world problems.",
+      degree: "Bachelor of Science - BSC (Mathematics)",
+    },
+    {
+      id: 2,
+      img: MHStateBoardLogo,
+      school: "Annapurna Devi Vidyalay Aral,TQ Basmat,Dist Hingoli",
+      date: "June 2014 - March 2015",
+      grade: "62.62%",
+      desc: "I completed my class 12th education from Annapurna Devi Vidyalay Aral, under the Maharashtra State board, where I studied Physics, Chemistry,Biology and Mathematics",
+      degree: "HSC",
+    },
+    {
+      id: 3,
+      img: MHStateBoardLogo,
+      school: "Annapurna Devi Vidyalay Aral,TQ Basmat,Dist Hingoli",
+      date: "June 2012 - March 2013",
+      grade: "63.84%",
+      desc: "I completed my class 10 education from Annapurna Devi Vidyalay Aral, under the Maharashtra State  board.",
+      degree: "SSC",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 0,
+      title: "Service-Hub",
+      description:
+        "ServiceHub is an interactive web application that brings multiple services onto a single platform, enabling seamless communication between users and service providers.",
+      image: ServiceHubLogo,
+      tags: ["HTML", "CSS", "JavaScript", "Jquery", "Java" , "Spring Framework" ,"Spring-boot" ,"JDBC" ,"MySQL"],
+      github: "https://github.com/AdinathIngole/Service-Hub-with-jsp",
+      webapp: null,
+    },
+    {
+      id: 1,
+      title: "Hotel-Management-Systems",
+      description:
+        "Built a full-stack Hotel Management System to simplify and automate hotel operations including room booking, customer management, billing, and administrative tasks. The platform is designed to be used by hotel staff and management to improve operational efficiency and customer satisfaction.",
+      image: HotelManagementLogo,
+      tags: ["Java", "Spring-boot", "Hibernate", "MySQL", "ReactJs","Bootstrap", "JavaScript", "RestAPI","JWT","Spring-Security","JDBC"],
+      github: "https://github.com/AdinathIngole/Hotel-Good-Time",
+      webapp: null,
+    },
+    {
+      id: 2,
+      title: "Matrimony Web Application",
+      description:
+        "Developed a secure and user-friendly full-stack Matrimony web application designed to help individuals create profiles, search for matches, and communicate with potential partners. The platform provides a seamless user experience for profile creation, browsing, shortlisting, and messaging, while ensuring data security and privacy.",
+      image: MatrimonyLogo,
+      tags: ["Java", "Spring-boot", "Hibernate", "MySQL", "ReactJs", "Bootstrap","JavaScript", "RestAPI","JWT","Spring-Security","JDBC"],
+      github: "https://github.com/AdinathIngole/Matrimony",
+      webapp: null,
+    },
+    {
+      id: 3,
+      title: "HRMS",
+      description:
+        "Designed and developed a comprehensive Human Resource Management System to streamline and automate core HR functions such as employee management, attendance tracking, leave management, and payroll processing. The system enhances HR productivity and ensures data accuracy across departments.",
+      image: HRMSLogo,
+      tags: ["Java", "Spring-boot", "MySQL", "ReactJs", "Bootstrap","JavaScript","JWT","Spring-Security"],
+      github: "https://github.com/AdinathIngole/HRMS",
+      webapp: null,
+    },
+    {
+      id: 4,
+      title: "SSMF-NGO",
+      description:
+        "This project is a full-stack web application developed for The Sant Sevalal Maharaj Foundation NGO with the goal of managing donations, volunteers, events, and outreach activities more efficiently.The application is built using Java Spring Boot and Hibernate on the backend, ensuring secure, scalable, and reliable server-side processing. The ReactJS frontend provides a modern, responsive, and user-friendly interface, while Axios is used for seamless API communication between frontend and backend. Data is stored and managed in a MySQL database, with structured relationships handled via Hibernate ORM.",
+      image: SSMFNGOLOGO,
+      tags: ["Java", "Spring-boot", "MySQL", "ReactJs", "Axios" ,"JavaScript","JWT","Spring-Security"],
+      github: "https://github.com/AdinathIngole/SSMF-NGO",
+      webapp: null,
+    },
+    {
+      id: 5,
+      title: "Event Management System",
+      description:
+        "Developed a full-stack Event Management System to streamline the organization and coordination of various events such as conferences, seminars, Birthday, family functions and workshops. The platform allows event organizers to create and manage events, while users can browse events, register, and receive updates.",
+      image: EventManagementLogo,
+      tags: ["Java", "Spring-boot", "MySQL", "ReactJs", "Bootstrap","JavaScript","RestAPI"],
+      github: "https://github.com/AdinathIngole/LakshareEventManagement",
+      webapp: null,
+    },
+    {
+      id: 6,
+      title: "Library Management System",
+      description:
+        "The Library Management System is a complete web-based application designed to simplify and automate the operations of a library. It is built using Spring Boot for the backend and MySQL as the database, with a JSP-based frontend styled with HTML, CSS, and JavaScript.The system provides librarians and users with an easy-to-use platform for managing library resources and activities.",
+      image: LibraryLogo,
+      tags: ["Java", "Spring-boot","Spring-MVC", "MySQL", "HTML","CSS","JSP","JavaScript"],
+      github: "https://github.com/AdinathIngole/Library-Management",
+      webapp: null,
+    },
+    {
+      id: 7,
+      title: "Company Portfolio",
+      description:
+        "The Kaushalya Tech Company Website is a dynamic and responsive full-stack web application developed using Spring Boot, Hibernate JPA, ReactJS, and MySQL. The project is designed to serve as the official website for Kaushalya Tech, showcasing its services, expertise, and company profile in a modern, user-friendly interface.",
+      image: CompanyLogo,
+      tags: ["Java", "Spring-boot", "MySQL", "ReactJs", "Bootstrap","JavaScript","Hibernate","JDBC"],
+      github: "https://github.com/AdinathIngole/kaushalyatechsolution_server",
+      webapp: null,
+    },
+  ];  
