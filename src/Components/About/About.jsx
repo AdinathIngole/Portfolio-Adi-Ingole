47827ef9ae50343a049cfd06fe6093d38936da1d
AdinathIngole <adiingole1006@gmail.com>
2025-11-13 15:39:53 +0530

    update

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -45,7 +45,7 @@ const About = () => {
 
           {/* Resume Button */}
           <a
-            href="https://drive.google.com/file/d/1eTpJVZNJGaQCl1dK1DTPMlgWDvt_ZQle/view?usp=sharing"
+            href="https://drive.google.com/file/d/1zxV7vnCq8mYM_m-K__F_6Z7pKXAr9Rbb/view?usp=sharing"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
